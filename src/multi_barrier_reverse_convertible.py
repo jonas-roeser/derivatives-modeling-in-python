b9bf5efa011ce8ee9aa84b5a6b1c070d04b257f0
jonas-roeser <accs.roeser@outlook.com>
2024-05-01 11:01:19 +0200

    create multi barrier reverse convertible script

---
+++ src/multi_barrier_reverse_convertible.py
@@ -0,0 +1,58 @@
+'''
+University of St.Gallen
+
+8,194: Derivatives Modeling in Python
+--- Case Study ---
+5 May 2024
+
+Dr. Mathis Moerke
+'''
+
+# Import packages
+# ...
+
+def sim_correlated_path():
+    '''
+    Simulate correlated stock price paths.
+
+    Parameters
+    ---
+    price : str, default None
+        File name.
+    vola : 
+        Vola of underlying.
+    rate : 
+        Interest rate (cont. comp., annualized).
+    t :
+        Entire time.
+    
+    Returns
+    ---
+    price : Series
+        Series of prices.
+    '''
+    # Insert code
+    pass
+
+def worst_of_down_and_in_put():
+    '''
+    Calcualte worst-of down-and-in put option price.
+    
+    Parameters
+    ---
+    ...
+
+    Returns
+    ---
+    ...
+    '''
+    # Insert code
+    pass
+
+if __name__ == '__main__':
+
+    # Simulate prices
+    sim_correlated_path()
+
+    # Calcualte worst-of down-and-in put option price
+    worst_of_down_and_in_put()
