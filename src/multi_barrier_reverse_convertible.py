3260bda34785e87384df9e4898c971db86e58d9c
jonas-roeser <accs.roeser@outlook.com>
2024-05-04 05:08:33 +0200

    implement implied volatility computation

--- src/multi_barrier_reverse_convertible.py
+++ src/multi_barrier_reverse_convertible.py
@@ -12,6 +12,8 @@ Dr. Mathis Moerke
 import numpy as np
 import pandas as pd
 import pandas_market_calendars as mcal
+from scipy.stats import norm
+from scipy.optimize import minimize
 
 def bond(nominal_value, coupon_rate, coupon_frequency, years_to_maturity, i_rate, credit_spread):
     '''
@@ -60,6 +62,167 @@ def bond(nominal_value, coupon_rate, coupon_frequency, years_to_maturity, i_rate
 
     return bond_price
 
+def black_scholes(S, K, t, T, v, r, dividend_yield, option_type):
+    '''
+    Compute Black-Scholes option price.
+
+    Parameters
+    ---
+    S : int or float
+        Spot price of the underlying at time t.
+    K : int or float
+        Strike price of the option.
+    t : pd.datetime
+        Pricing date.
+    T : pd.datetime
+        Expiration date.
+    volatility : int or float
+        Volatility of the underlying (continuous & annualised).
+    r : int or float
+        Risk free rate to maturity (continuous & annualised).
+    dividend_yield : float
+        Dividend by share price.
+    option_type : str
+        Variable indicating call or put option, one of 'C' or 'P'.
+
+    Returns
+    ---
+    option_price :
+        Price of the option.
+    '''
+    # Create parameter dictionary
+    parameters = {'S': S, 'K': K, 't': t, 'T': T, 'v': v, 'r': r, 'dividend_yield': dividend_yield, 'option_type': option_type}
+
+    # Ensure all parameters except volatility and expiration date are of type Series
+    parameters = {key: value if isinstance(value, pd.core.series.Series) else pd.Series(value) for key, value in parameters.items()}
+
+    # Check if all parameters except volatility and expiration date have the same length
+    if any(len(value) != len(parameters['S']) for key, value in parameters.items() if key not in {'T', 'v'}):
+        raise ValueError('Option parameters are of different length')
+    
+    # Initialise list of option prices
+    option_prices = []
+
+    # Iteratre through all options
+    for i in range(len(S)):
+
+        # Set parameters for each option
+        S = parameters['S'].iloc[i]
+        K = parameters['K'].iloc[i]
+        t = parameters['t'].iloc[i]
+        T = parameters['T'].iloc[0]
+        v = parameters['v'].iloc[0]
+        r = parameters['r'].iloc[i]
+        dividend_yield = parameters['dividend_yield'].iloc[i]
+        option_type = parameters['option_type'].iloc[i]
+
+        # Compute number of days from pricing to expiration date
+        n_days = (T - t).days + 1 # Should this be in trading days?
+
+        # Compute time to maturity T in years
+        T_t = n_days / 365.25
+
+        # Set call_put flag
+        if option_type == 'C':
+            call_put = 1
+        elif option_type == 'P':
+            call_put = -1
+        else:
+            raise ValueError('Invalid option type')
+
+        # Check if option parameters are valid
+        if S > 0 and K > 0 and T_t > 0 and v > 0 and r >= 0 and dividend_yield >= 0:
+
+            # Compute Black-Scholes probability factor
+            d = (np.log(S / K) + (r - dividend_yield + v**2 / 2) * T_t) / (v * np.sqrt(T_t))
+
+            # Compute Black-Scholes option price
+            option_price = call_put * S * np.exp(-dividend_yield * T_t) * norm.cdf(call_put * d) - call_put * K * np.exp(-r * T_t) * norm.cdf(call_put * (d - v * np.sqrt(T_t)))
+        
+        # Check if any of S, K, T_t or v are 0
+        elif S == 0 or K == 0 or T_t == 0 or v == 0:
+        
+            # Set option price equal to ...
+            option_price = max(call_put * (S * np.exp(-dividend_yield * T_t) - K * np.exp(-r * T_t)), 0)
+
+        # Catch invalid option parameters
+        else:
+            raise ValueError('Invalid option parameters')
+        
+        # Append option price to list of option prices
+        option_prices.append(option_price)
+
+    return option_prices
+
+def rmse(actuals, error_type='absolute', **kwargs):
+    '''
+    Compute root mean squared error.
+
+    Parameters
+    ---
+    actuals : int or float
+        Actual value.
+    predicted : int or float
+        Predicted value.
+    error_type : str, default 'absolute'
+        Type of error, one of 'absolute' or 'relative'
+
+    Returns
+    ---
+    error :
+        Root mean squared error.
+    '''
+    # Initialise list of errors
+    errors = []
+
+    # Compute Black-Scholes option prices
+    predictions = black_scholes(**kwargs)
+
+    # Compute error for each option
+    for actual, prediction in zip(actuals, predictions):
+
+        # Compute absolute error
+        error = prediction - actual
+        
+        # For relative error type transform into relative terms
+        if error_type == 'absolute':
+            pass
+        elif error_type == 'relative':
+            error = error / actual
+        else:
+            raise ValueError('Invalid error type')
+        
+        errors.append(error)
+    
+    # Compute root mean squared error
+    error = np.sqrt(np.mean(error**2))
+    
+    return error
+
+def implied_volatility(error_function=rmse, starting_value=0.2, method='Nelder-Mead', bounds=(0, None), **kwargs):
+    '''
+    Compute option-implied volatility.
+
+    Parameters
+    ---
+    error_function : callable, default rmse
+        Function for computing the error that is to be minimised.
+    starting_value : int or float, default 0
+        Starting value of the estimator.
+    
+    Returns
+    ---
+    iv : float
+        Option implied volatility.
+    '''
+    
+    # Optimise function by minimising error
+    optimisation_result = minimize(lambda x: error_function(v=x, **kwargs), starting_value, method=method, bounds=[bounds])
+    
+    # Extract implied volatility
+    iv = optimisation_result.x[0]
+
+    return iv
 
 def sim_correlated_paths(underlying_prices, volatilities, pricing_date=None, expiration_date=None, i_rate=0.01, exchange='SIX'):
     '''
@@ -254,11 +417,39 @@ if __name__ == '__main__':
     I_RATE = 0.01
     volatilities = pd.DataFrame([[0.2, 0.25, 0.15]], columns=stock_prices.columns)
 
-    # nominal_value = 1000
-    # annual_coupon_rate = 0.085
-    # maturity_in_years = 1
-    # r = 0.0277094 # 1 Year US Forward Rate as of 26.06.2018
-    # credit_risk_spread = 0.00172599899999999 # 1 year Unsubordinated UBS cds as of 26.06.2018
+    # Add stock price to option data
+    stock_prices_long = stock_prices.melt(var_name='underlying', value_name='underlying_price', ignore_index=False)
+    option_prices = pd.merge(option_prices, stock_prices_long, on=['date','underlying'])
+
+    # Add risk free rate to option data
+    option_prices['rate'] = 0.1
+
+    # Add dividend yield to option data
+    option_prices['dividend_yield'] = 0
+
+    option_prices['volatility'] = option_prices.groupby('exdate').apply(lambda option: implied_volatility(
+        error_function=rmse, starting_value=0.2, method='Nelder-Mead', bounds=(0, None), # kwargs for implied_volatility()
+        actuals=option.option_price, error_type='absolute', # kwargs for rmse()
+        S=option.underlying_price, K=option.strike, t=option.index, T=option.name, r=option.rate, dividend_yield=option.dividend_yield, option_type=option.option_type)) # kwargs for black_scholes()
+
+    # # Calculate implied volatility for each option and add it as a new column
+    # options['implied_volatility'] = options.apply(
+    #     lambda row: implied_volatility(
+    #         row['option_price'], row['S'], row['K'], row['T'], r, row['option_type']
+    #     ), axis=1
+    # )
+
+    # # Filter data for options as of June 26, 2018
+    # specific_date = pd.Timestamp('2018-06-26')
+    # one_year_options = options[
+    #     (options['date'] == specific_date) &
+    #     (options['T'].between(0.9, 1.1))
+    # ]
+
+    # # Get ATM implied volatility for each stock
+    # adobe_iv = get_atm_iv(atm_options, 'Adobe')
+    # apple_iv = get_atm_iv(atm_options, 'Apple')
+    # microsoft_iv = get_atm_iv(atm_options, 'Microsoft')
 
     # Calculate bond price
     bond_price = bond(NOMINAL_VALUE, COUPON_RATE, COUPON_FREQUENCY, MATURITY_IN_YEARS, i_rate, credit_spread)
